6cfa3bedcf379ad09124043510528c847ab84450
COREY COSMAN <cosman.corey@gmail.com>
2017-11-24 16:38:12 -0500

    added google map JS API, added styles for height and width of map on events stylesheet, added js scripts on events.js, added js script on event show page

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+  function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var bitmaker = {lat: 43.647649, lng: -79.387082}
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: bitmaker,
+      zoom: 15
+    });
+    var marker = new google.maps.Marker({
+     map: map,
+     position: "M5H 1K4",
+     title: 'Hello World!'
+   });
+
+  }
