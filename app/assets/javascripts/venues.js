4da769790e830dc8857fcabe314ada7493bd1767
marmo002 <marmo_02@hotmail.com>
2017-11-25 18:19:47 -0500

    populated map on venue index with all venues markers

--- app/assets/javascripts/venues.js
+++ app/assets/javascripts/venues.js
@@ -1,2 +1,40 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function initMap() {
+
+  // var map = new google.maps.Map(document.getElementById('venues_map'), {
+  //   center: {lat: 43.650325, lng: -79.383795},
+  //   zoom: 15
+  // });
+
+  // Create a map object and specify the DOM element for display.
+  $.ajax({
+    url: document.URL,
+    method:'GET',
+    dataType: 'JSON'
+  }).done(function(response){
+
+    coord = {lat: response[0]["lat"], lng: response[0]["lng"]};
+
+    var map = new google.maps.Map(document.getElementById('venues_map'), {
+      center: coord,
+      zoom: 13
+    });
+
+
+    response.forEach(function(venue) {
+        new google.maps.Marker({
+          map: map,
+          position: { lat: venue["lat"], lng: venue["lng"] },
+          title: venue["venueName"]
+
+         });
+
+    });
+
+
+
+  })
+
+
+}
