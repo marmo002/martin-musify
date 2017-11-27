d3fedc5186a773c6decfc482d79fa1634e479666
marmo002 <marmo_02@hotmail.com>
2017-11-27 11:14:52 -0500

    On genre show page, added google map with markers for each event

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,5 +5,28 @@ class GenresController < ApplicationController
 
   def show
     @genre = Genre.find(params[:id])
+    @events  = @genre.events
+
+    respond_to do |format|
+      format.html
+      format.json do
+
+        json_data = @events.map do |event|
+          {
+            "eventId": event.id,
+            "eventName": event.name,
+            "address_1": event.venue.address_1,
+            "city": event.venue.city,
+            "postal_code": event.venue.postal_code,
+            "lat": event.venue.latitude,
+            "lng": event.venue.longitude
+          }
+        end
+
+        render json: json_data
+      end
+    end#respond_to
+
+
   end
 end
