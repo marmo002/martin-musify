764dae2ca19c5a4f6f1b039564f2fa06522efe68
Tony <ANCLIST@GMAIL.COM>
2017-11-28 12:14:28 -0500

    Views to create user and to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    if @user = User.save(user_params)
-      auto_login(@user)
-      redirect_to root_url
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Thank you for signing up'
     else
       render 'new'
     end
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
