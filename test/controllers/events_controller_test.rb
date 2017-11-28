f178591e5cf9035b1ddd14cb8940ccbb9a16f154
Zach Job <zachery.job@gmail.com>
2017-11-28 16:46:34 -0500

    Setup event factory and event controller tests. Events index test currently failing

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
+require 'factory_bot'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get index" do
+    event = create(:event)
+    get events_url
+    assert_response :success
+  end
+  
+  test "should get show" do
+    event = create(:event)
+    get event_url(event)
+    assert_response :success
+  end
+
 end
