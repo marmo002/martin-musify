e51bd7c0ac9cd72a100c98b8fcbc8c7e5e99e25b
Tony <ANCLIST@GMAIL.COM>
2017-11-27 20:53:30 -0500

    Display flex for card views on Event Index Genre Show and Venue Index, Event Show View with Jumbotron

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,5 @@ class Image < ApplicationRecord
   belongs_to :event
 
   scope :sixteen_by_nine, -> { where(ratio: "16_9").where("url like ?", '%RETINA%') }
+  scope :four_by_three, -> { where(ratio: "4_3").where("url like ?", '%CUSTOM%') }
 end
