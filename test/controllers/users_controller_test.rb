6f4e7ef6521692d2748dd43db5ec41ed82783f5f
Zach Job <zachery.job@gmail.com>
2017-11-29 17:46:10 -0500

    Remove create-user test in controller. Correct new user url to make test pass.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,15 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
-    skip
-    get users_new_url
-    assert_response :success
-  end
-
-
-  test "should get create" do
-    skip
-    get users_create_url
+    get new_user_url
     assert_response :success
   end
 
