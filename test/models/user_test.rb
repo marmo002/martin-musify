a16fd0b7fd047ed531715a25c05e7fd40729e3bc
marmo002 <marmo_02@hotmail.com>
2017-11-29 12:36:40 -0500

    user model testing. Note finish with it yet

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
+require 'byebug'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "test email uniqueness returns false" do
+    # byebug
+    user1 = create(:user)
+    user2 = build(:user)
+    refute user2.valid?
+  end
+
+  test "test email uniqueness returns true" do
+    # byebug
+    user1 = create(:user)
+    user2 = build(:user, email: 'mail2@mail.com')
+    assert user2.valid?
+  end
+
+  test "test email uniqueness returns true" do
+    user1 = create(:user)
+    user2 = build(:user, email: 'mail2@mail.com')
+    assert user2.valid?
+  end
 end
