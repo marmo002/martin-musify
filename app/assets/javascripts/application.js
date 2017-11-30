216c9034c16a7d76954c4d82592c72e96255a88a
marmo002 <marmo_02@hotmail.com>
2017-11-30 11:42:40 -0500

    Add content to my abt page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,9 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
+
+
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    var scroll2 = document.body.scrollTop = 0; // For Safari
+    var scroll1 = document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
