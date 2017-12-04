d2bf0834b5d522c743c31c53058bdb8cf6d3086c
marmo002 <marmo_02@hotmail.com>
2017-12-04 14:51:20 -0500

    Added user profile page. On user profile, added user info and edit link for user to edit name, email and password

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,15 @@ root "genres#index"
 get "venues/location"
 get "genres/location"
 post "events/location"
+get 'profile' => 'users#profile', :as => :profile
 
-  
+
 resources :events, except: :destroy
 resources :venues, only: [:index, :show]
 resources :artists, only: [:show]
 resources :genres, only: [:index, :show]
 resources :abouts, only: [:index]
-resources :users, only: [:new, :create]
+resources :users, only: [:new, :create, :edit, :update]
 get 'login' => 'user_sessions#new', :as => :login
 delete 'logout' => 'user_sessions#destroy', :as => :logout
 resources :user_sessions, only: [:create]
