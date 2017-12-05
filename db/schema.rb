a17a8f3f8b621c2da82613e19c29a82071f2861b
Lan Phan <lanc_phan@hotmail.com>
2017-12-05 12:23:57 -0500

    made a join table migration between genre and user. Add the heart icon onto each genre card. Add association relationshps.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204202147) do
+ActiveRecord::Schema.define(version: 20171205165804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,11 @@ ActiveRecord::Schema.define(version: 20171204202147) do
     t.datetime "image_updated_at"
     t.index ["artist_id"], name: "index_events_on_artist_id"
     t.index ["venue_id"], name: "index_events_on_venue_id"
+  end
+
+  create_table "favourites", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "genre_id", null: false
   end
 
   create_table "genres", force: :cascade do |t|
