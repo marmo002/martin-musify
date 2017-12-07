ab9cc9323a0364f72e00ae9d33e439af82851568
coreycosman <cosman.corey@gmail.com>
2017-12-07 18:57:46 -0500

    create test for genre model (genre factory is being created)

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 require 'factory_bot'
 class GenreTest < ActiveSupport::TestCase
 
-  test ""
+  test "create genre valid is true" do
+    genre = create(:genre)
+
+    assert genre.valid?
+
+  end
+
 
 
 
