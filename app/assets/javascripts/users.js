5cfe8106156fdd8ab86f07f7f977130f5c91f6af
Lan Phan <lanc_phan@hotmail.com>
2017-12-07 19:27:07 -0500

    delete the genre in the user's profile page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var favouriteGenreGroup = document.querySelector('.favourite-list');
+
+  favouriteGenreGroup.addEventListener('click',function(e) {
+    if (e.target.classList.contains('delete-genre')) {
+      e.preventDefault();
+      e.target.parentNode.remove();
+    }
+
+  })
+});
