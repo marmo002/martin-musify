d0a99f1d136fcf6040eedeb6e6c1b42b504873bf
Tony <ANCLIST@GMAIL.COM>
2017-12-07 19:27:40 -0500

    Set ENV variable for lastfm api key

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,24 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-document.addEventListener('DOMContentLoaded', function() {
-  var artistName = document.getElementById('artist-name').innerText
-  var token = "d6fbb8c1f037a6f0f2ba5b3af409099c"
-
-  $.ajax({
-    url: `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${token}&format=json`,
-    method: 'GET',
-    dataType: 'JSON'
-  }).done(function(responseData) {
-    console.log(responseData);
-
-    var imageColumn = document.getElementById('image-column');
-    var artistImage = document.createElement("img");
-    var imageSRC = responseData['artist']['image'][3]['#text'];
-
-    artistImage.setAttribute("src", imageSRC);
-    artistImage.setAttribute("class", 'img-fluid');
-    imageColumn.appendChild(artistImage)
-  })
-
-})
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,5 +4,13 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+
+    token = ENV["LASTFM_KEY"]
+
+    response = HTTParty.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{@artist.name}&api_key=#{token}&format=json")
+    body = JSON.parse(response.body)
+
+    @src = body['artist']['image'][3]['#text']
+
   end
 end
