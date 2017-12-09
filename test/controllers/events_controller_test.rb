6bf44d54e3274442f2d2db823a816f54fea61a0b
coreycosman <cosman.corey@gmail.com>
2017-12-09 17:58:12 -0500

    fix broken controller test by adding condition on line 24 of event show

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -8,21 +8,14 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     event1 = create(:event)
     event1.venue = venue1
     event1.save
+    
     get events_url
     assert_response :success
   end
-  # test "should not get index" do
-  #   venue1 = create(:venue)
-  #   event1 = create(:event)
-  #   event1.venue = venue1
-  #   event1.save
-  #   get events_url
-  #   assert_response :success
-  # end
 
   test "should get show" do
-    skip
     event = create(:event)
+
     get event_url(event)
     assert_response :success
   end
